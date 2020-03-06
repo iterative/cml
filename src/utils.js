57db251d74a4529dfa1adffb79ef16a628950d5e
davidgortega <g.ortega.david@gmail.com>
2020-03-06 16:13:50 +0100

    check exec error

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,11 @@ const git = require('simple-git/promise');
 const execp = util.promisify(require('child_process').exec);
 const exec = async (command, opts) => {
   const { debug, throw_err = true } = opts || {};
-  const { stdout, stderr } = await execp(command);
+  const { error, stdout, stderr } = await execp(command);
 
   if (debug) console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
 
-  if (throw_err && stderr) throw new Error(stderr);
+  if (throw_err && error) throw error;
 
   return stdout;
 };
