7f6d168d93c4d54b3e7918124128cf406af78d24
DavidGOrtega <g.ortega.david@gmail.com>
2020-05-22 12:57:10 +0200

    Tensorboard (#103)
    * tests and code (#98)
    
    * code&tests
    
    * workflow

---
+++ bin/cml-tensorboard-dev.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+const print = console.log;
+console.log = console.error;
+
+const yargs = require('yargs');
+const fs = require('fs').promises;
+const { spawn } = require('child_process');
+const { homedir } = require('os');
+const { exec } = require('../src/utils');
+
+const { handle_error } = process.env.GITHUB_ACTION
+  ? require('../src/github')
+  : require('../src/gitlab');
+
+const { TB_CREDENTIALS } = process.env;
+
+const run = async opts => {
+  const {
+    md,
+    file,
+    credentials = TB_CREDENTIALS,
+    logdir,
+    name,
+    description
+  } = opts;
+
+  // set credentials
+  const path = `${homedir()}/.config/tensorboard/credentials`;
+  await fs.mkdir(path, { recursive: true });
+  await fs.writeFile(`${path}/uploader-creds.json`, credentials);
+
+  // launch  tensorboard on background
+  const tb_path = await exec('which tensorboard');
+  const help = await exec('tensorboard dev upload -h');
+  const extra_params_found =
+    (name || description) && help.indexOf('--description') >= 0;
+  const extra_params = extra_params_found
+    ? `--name "${name}" --description "${description}"`
+    : '';
+  const command = `python -u ${tb_path} dev upload --logdir ${logdir} ${extra_params}`;
+  console.log(command);
+
+  const proc = spawn(command, {
+    detached: true,
+    shell: true
+  });
+
+  proc.stderr.on('data', data => {
+    data && console.error(data.toString('utf8'));
+  });
+
+  let output = '';
+  proc.stdout.on('data', async data => {
+    if (data) {
+      console.error(data.toString('utf8'));
+
+      output += data.toString('utf8');
+
+      const uri_index = output.indexOf('https://', 0);
+      if (uri_index) {
+        output = output.substring(uri_index, output.length - 1);
+
+        if (md) output = `[${name}](${output})`;
+
+        if (!file) print(output);
+        else await fs.writeFile(file, output);
+
+        process.exit(0);
+      }
+    }
+  });
+  proc.unref();
+};
+
+const argv = yargs
+  .usage(`Usage: $0 <path>`)
+  .default('credentials')
+  .alias('c', 'credentials')
+  .default('logdir', '', 'Directory containing the logs to process')
+  .default('name', '', 'Title of the experiment. Max 100 characters.')
+  .default(
+    'description',
+    '',
+    'Experiment description. Markdown format. Max 600 characters.'
+  )
+  .default('plugins')
+  .boolean('md')
+  .alias('name', 'title')
+  .alias('t', 'title')
+  .help('h').argv;
+run(argv).catch(e => handle_error(e));
---
+++ bin/cml-tensorboard-dev.test.js
@@ -0,0 +1,44 @@
+jest.setTimeout(200000);
+
+const { exec } = require('../src/utils');
+
+describe('CML e2e', () => {
+  test('cml-tensorboard-dev.js -h', async () => {
+    const output = await exec(`node ./bin/cml-tensorboard-dev.js -h`);
+
+    expect(output).toMatchInlineSnapshot(`
+      "Usage: cml-tensorboard-dev.js <path>
+
+      Options:
+        --version        Show version number                                 [boolean]
+        -h               Show help                                           [boolean]
+        --logdir                   [default: Directory containing the logs to process]
+        --name, --title        [default: Title of the experiment. Max 100 characters.]
+        --description
+               [default: Experiment description. Markdown format. Max 600 characters.]
+        --plugins"
+    `);
+  });
+
+  test('cml-tensorboard-dev.js --md', async () => {
+    const credentials =
+      '{"refresh_token": "1//03IJCTMThPsYECgYIARAAGAMSNwF-L9Iru6PoxuqEtGTcnvbXeGwi5j4cXBrFQpXdcmBhZyvZggR1WqKeIjhs1V57g1NvpCsUFnw", "token_uri": "https://oauth2.googleapis.com/token", "client_id": "373649185512-8v619h5kft38l4456nm2dj4ubeqsrvh6.apps.googleusercontent.com", "client_secret": "pOyAuU2yq2arsM98Bw5hwYtr", "scopes": ["openid", "https://www.googleapis.com/auth/userinfo.email"], "type": "authorized_user"}';
+    const name = 'My experiment';
+    const output = await exec(
+      `node ./bin/cml-tensorboard-dev.js --md --credentials '${credentials}' --logdir logs --name '${name}' --description 'Test experiment'`
+    );
+
+    expect(output.startsWith(`[${name}](https://`)).toBe(true);
+  });
+
+  test('cml-tensorboard-dev.js', async () => {
+    const credentials =
+      '{"refresh_token": "1//03IJCTMThPsYECgYIARAAGAMSNwF-L9Iru6PoxuqEtGTcnvbXeGwi5j4cXBrFQpXdcmBhZyvZggR1WqKeIjhs1V57g1NvpCsUFnw", "token_uri": "https://oauth2.googleapis.com/token", "client_id": "373649185512-8v619h5kft38l4456nm2dj4ubeqsrvh6.apps.googleusercontent.com", "client_secret": "pOyAuU2yq2arsM98Bw5hwYtr", "scopes": ["openid", "https://www.googleapis.com/auth/userinfo.email"], "type": "authorized_user"}';
+    const name = 'My experiment';
+    const output = await exec(
+      `node ./bin/cml-tensorboard-dev.js --credentials '${credentials}' --logdir logs --name '${name}' --description 'Test experiment'`
+    );
+
+    expect(output.startsWith(`https://`)).toBe(true);
+  });
+});
