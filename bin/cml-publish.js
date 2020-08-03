cee1b148bab94c2ba8e4b4f2f10bc95bfe9aace2
Restyled.io <commits@restyled.io>
2020-08-03 16:58:05 +0000

    Restyled by prettier

--- bin/cml-publish.js
+++ bin/cml-publish.js
@@ -6,35 +6,33 @@ console.log = console.error;
 const fs = require('fs').promises;
 const pipe_args = require('../src/pipe-args');
 const yargs = require('yargs');
-const {publish_file} = require('../src/report');
+const { publish_file } = require('../src/report');
 
-const {handle_error} = process.env.GITHUB_ACTIONS ? require('../src/github')
-                                                  : require('../src/gitlab');
+const { handle_error } = process.env.GITHUB_ACTIONS
+  ? require('../src/github')
+  : require('../src/gitlab');
 
-const run = async opts => {
-  const {file, md, title} = opts;
+const run = async (opts) => {
+  const { file, md, title } = opts;
   const path = opts._[0];
 
   let buffer;
-  if (data)
-    buffer = Buffer.from(data, 'binary');
+  if (data) buffer = Buffer.from(data, 'binary');
 
-  const output = await publish_file({path, buffer, md, title});
-  if (!file)
-    print(output);
-  else
-    await fs.writeFile(file, output);
+  const output = await publish_file({ path, buffer, md, title });
+  if (!file) print(output);
+  else await fs.writeFile(file, output);
 };
 
 pipe_args.load('binary');
 const data = pipe_args.piped_arg();
-const argv = yargs.usage(`Usage: $0 <path> --file <string>`)
-                 .boolean('md')
-                 .default('title')
-                 .alias('t', 'title')
-                 .default('file')
-                 .alias('f', 'file')
-                 .help('h')
-                 .demand(data ? 0 : 1)
-                 .argv;
-run({...argv, data}).catch(e => handle_error(e));
+const argv = yargs
+  .usage(`Usage: $0 <path> --file <string>`)
+  .boolean('md')
+  .default('title')
+  .alias('t', 'title')
+  .default('file')
+  .alias('f', 'file')
+  .help('h')
+  .demand(data ? 0 : 1).argv;
+run({ ...argv, data }).catch((e) => handle_error(e));
--- bin/cml-send-comment.js
+++ bin/cml-send-comment.js
@@ -5,22 +5,22 @@ console.log = console.error;
 const fs = require('fs').promises;
 const yargs = require('yargs');
 
-const {head_sha : HEAD_SHA, handle_error, comment} =
-    process.env.GITHUB_ACTIONS ? require('../src/github')
-                               : require('../src/gitlab');
+const { head_sha: HEAD_SHA, handle_error, comment } = process.env.GITHUB_ACTIONS
+  ? require('../src/github')
+  : require('../src/gitlab');
 
-const run = async opts => {
-  const {'head-sha' : head_sha = HEAD_SHA} = opts;
+const run = async (opts) => {
+  const { 'head-sha': head_sha = HEAD_SHA } = opts;
   const path = opts._[0];
   const report = await fs.readFile(path, 'utf-8');
 
-  await comment({head_sha, report});
+  await comment({ head_sha, report });
 };
 
-const argv = yargs.usage(`Usage: $0 <path> --head-sha <string>`)
-                 .default('head-sha')
-                 .describe('head-sha', 'Commit sha')
-                 .help('h')
-                 .demand(1)
-                 .argv;
-run(argv).catch(e => handle_error(e));
+const argv = yargs
+  .usage(`Usage: $0 <path> --head-sha <string>`)
+  .default('head-sha')
+  .describe('head-sha', 'Commit sha')
+  .help('h')
+  .demand(1).argv;
+run(argv).catch((e) => handle_error(e));
