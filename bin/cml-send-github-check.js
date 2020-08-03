bf18299faee9541f44fefb1db1b136bcb9b45eff
Restyled.io <commits@restyled.io>
2020-08-03 16:58:03 +0000

    Restyled by clang-format

--- bin/cml-send-github-check.js
+++ bin/cml-send-github-check.js
@@ -5,42 +5,29 @@ console.log = console.error;
 const fs = require('fs').promises;
 const yargs = require('yargs');
 
-const {
-  head_sha: HEAD_SHA,
-  handle_error,
-  create_check_report,
-  CHECK_TITLE
-} = process.env.GITHUB_ACTIONS
-  ? require('../src/github')
-  : require('../src/gitlab');
+const {head_sha : HEAD_SHA, handle_error, create_check_report, CHECK_TITLE} =
+    process.env.GITHUB_ACTIONS ? require('../src/github')
+                               : require('../src/gitlab');
 
 const run = async opts => {
-  const { 'head-sha': head_sha = HEAD_SHA, conclusion, title } = opts;
+  const {'head-sha' : head_sha = HEAD_SHA, conclusion, title} = opts;
   const path = opts._[0];
   const report = await fs.readFile(path, 'utf-8');
 
-  await create_check_report({
-    head_sha,
-    report,
-    conclusion,
-    title
-  });
+  await create_check_report({head_sha, report, conclusion, title});
 };
 
-const argv = yargs
-  .usage(`Usage: $0 <path> --head-sha <string>`)
-  .default('head-sha')
-  .default('conclusion', 'success')
-  .choices('conclusion', [
-    'success',
-    'failure',
-    'neutral',
-    'cancelled',
-    'skipped',
-    'timed_out'
-  ])
-  .default('title', CHECK_TITLE)
-  .describe('head-sha', 'Commit sha')
-  .help('h')
-  .demand(1).argv;
+const argv = yargs.usage(`Usage: $0 <path> --head-sha <string>`)
+                 .default('head-sha')
+                 .default('conclusion', 'success')
+                 .choices('conclusion',
+                          [
+                            'success', 'failure', 'neutral', 'cancelled',
+                            'skipped', 'timed_out'
+                          ])
+                 .default('title', CHECK_TITLE)
+                 .describe('head-sha', 'Commit sha')
+                 .help('h')
+                 .demand(1)
+                 .argv;
 run(argv).catch(e => handle_error(e));
