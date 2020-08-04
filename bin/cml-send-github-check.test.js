f6fa95c84b91dad3eca0f36bb80dcf9d90b5ccb9
Restyled.io <commits@restyled.io>
2020-08-04 10:58:33 +0000

    Restyled by prettier

--- bin/cml-send-github-check.test.js
+++ bin/cml-send-github-check.test.js
@@ -1,37 +1,43 @@
 jest.setTimeout(200000);
 
-const {exec} = require('../src/utils');
+const { exec } = require('../src/utils');
 const fs = require('fs').promises;
-const {publish_file} = require('../src/report');
+const { publish_file } = require('../src/report');
 
 describe('CML e2e', () => {
   test('cml-send-github-check', async () => {
     const path = 'check.md';
-    const img = await publish_file(
-        {path : 'assets/logo.png', md : true, title : 'logo'});
-    const pdf = await publish_file(
-        {path : 'assets/logo.pdf', md : true, title : 'logo'});
+    const img = await publish_file({
+      path: 'assets/logo.png',
+      md: true,
+      title: 'logo'
+    });
+    const pdf = await publish_file({
+      path: 'assets/logo.pdf',
+      md: true,
+      title: 'logo'
+    });
     const report = `## Test Check Report \n ${img} \n ${pdf}`;
 
     await fs.writeFile(path, report);
     process.env.GITHUB_ACTIONS &&
-        (await exec(`node ./bin/cml-send-github-check.js ${path}`));
+      (await exec(`node ./bin/cml-send-github-check.js ${path}`));
     await fs.unlink(path);
   });
 
-  test('cml-send-github-check failure with tile "CML neutral test"',
-       async () => {
-         const path = 'check.md';
-         const report = `## Hi this check should be neutral`;
-         const title = 'CML neutral test';
-         const conclusion = 'neutral';
+  test('cml-send-github-check failure with tile "CML neutral test"', async () => {
+    const path = 'check.md';
+    const report = `## Hi this check should be neutral`;
+    const title = 'CML neutral test';
+    const conclusion = 'neutral';
 
-         await fs.writeFile(path, report);
-         process.env.GITHUB_ACTIONS &&
-             (await exec(`node ./bin/cml-send-github-check.js ${
-                 path} --title "${title}" --conclusion "${conclusion}"`));
-         await fs.unlink(path);
-       });
+    await fs.writeFile(path, report);
+    process.env.GITHUB_ACTIONS &&
+      (await exec(
+        `node ./bin/cml-send-github-check.js ${path} --title "${title}" --conclusion "${conclusion}"`
+      ));
+    await fs.unlink(path);
+  });
 
   test('cml-send-github-check -h', async () => {
     const output = await exec(`node ./bin/cml-send-github-check.js -h`);
