9870c1cc93835d43ecc9502d7ac51aa1db43508c
davidgortega <g.ortega.david@gmail.com>
2020-08-06 16:18:46 +0200

    CML Github Action

---
+++ src/github-action.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const { exec } = require('./utils');
+
+const setup_cml = async () => {
+  const version = core.getInput('version');
+  core.info(`Intalling CML version ${version}`);
+  await exec(`npm i @dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`);
+};
+
+const setup_dvc = async () => {
+  const install = core.getInput('dvc_install');
+  const version = core.getInput('dvc_version');
+
+  if (install) {
+    core.info(`Intalling DVC version ${version}`);
+    await exec(
+      `pip install dvc[all]${version !== 'latest' ? `==${version}` : ''}`
+    );
+  }
+};
+
+(async () => {
+  try {
+    await setup_cml();
+    await setup_dvc();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
