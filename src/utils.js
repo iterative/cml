6dcbd08942182e16232a43254bf491fa13758e82
Restyled.io <commits@restyled.io>
2020-08-06 13:20:07 +0000

    Restyled by clang-format

--- src/utils.js
+++ src/utils.js
@@ -8,12 +8,14 @@ const isSvg = require('is-svg');
 const execp = util.promisify(require('child_process').exec);
 const exec = async (command, opts) => {
   return new Promise(function(resolve, reject) {
-    const { debug } = opts || {};
+    const {debug} = opts || {};
 
     execp(command, (error, stdout, stderr) => {
-      if (debug) console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
+      if (debug)
+        console.log(`\nCommand: ${command}\n\t${stdout}\n\t${stderr}`);
 
-      if (error) reject(error);
+      if (error)
+        reject(error);
 
       resolve((stdout || stderr).slice(0, -1));
     });
@@ -21,30 +23,31 @@ const exec = async (command, opts) => {
 };
 
 const mime_type = async opts => {
-  const { path, buffer } = opts;
+  const {path, buffer} = opts;
 
   try {
-    const svg_cadidate = path
-      ? await fs.promises.readFile(path)
-      : buffer.toString('utf-8');
+    const svg_cadidate =
+        path ? await fs.promises.readFile(path) : buffer.toString('utf-8');
 
-    if (isSvg(svg_cadidate)) return 'image/svg+xml';
+    if (isSvg(svg_cadidate))
+      return 'image/svg+xml';
 
     let mime;
-    if (path) ({ mime } = await FileType.fromFile(path));
-    else ({ mime } = await FileType.fromBuffer(buffer));
+    if (path)
+      ({mime} = await FileType.fromFile(path));
+    else
+      ({mime} = await FileType.fromBuffer(buffer));
 
     return mime;
   } catch (err) {
     console.log(err);
     throw new Error(
-      `Failed guessing mime type of ${path ? `file ${path}` : `buffer`}`
-    );
+        `Failed guessing mime type of ${path ? `file ${path}` : `buffer`}`);
   }
 };
 
 const upload = async opts => {
-  const { path, buffer } = opts;
+  const {path, buffer} = opts;
   const endpoint = 'https://asset.cml.dev';
   const mime = await mime_type(opts);
 
@@ -54,7 +57,7 @@ const upload = async opts => {
 
   if (path) {
     body = fs.createReadStream(path);
-    ({ size } = await fs.promises.stat(path));
+    ({size} = await fs.promises.stat(path));
     filename = PATH.basename(path);
   } else {
     body = buffer;
@@ -63,25 +66,19 @@ const upload = async opts => {
   }
 
   const headers = {
-    'Content-length': size,
-    'Content-Type': mime,
-    'Content-Disposition': `inline; filename="${filename}"`
+    'Content-length' : size,
+    'Content-Type' : mime,
+    'Content-Disposition' : `inline; filename="${filename}"`
   };
-  const response = await fetch(endpoint, { method: 'POST', headers, body });
+  const response = await fetch(endpoint, {method : 'POST', headers, body});
   const uri = await response.text();
 
-  return { mime, size, uri };
+  return {mime, size, uri};
 };
 
 const randid = () => {
-  return (
-    Math.random()
-      .toString(36)
-      .substring(2, 7) +
-    Math.random()
-      .toString(36)
-      .substring(2, 7)
-  );
+  return (Math.random().toString(36).substring(2, 7) +
+          Math.random().toString(36).substring(2, 7));
 };
 
 exports.exec = exec;
