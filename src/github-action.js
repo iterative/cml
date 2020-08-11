c7a13413ff733c6d0f014eb59f2a5c7e411a166d
davidgortega <g.ortega.david@gmail.com>
2020-08-11 11:14:13 +0200

    fix equal

--- src/github-action.js
+++ src/github-action.js
@@ -6,23 +6,15 @@ const setup_cml = async () => {
 
   try {
     core.info('Unintalling previous CML');
-    await exec('sudo npm uninstall -g @dvcorg/cml');
-  } catch (err) {
-    try {
-      await exec('npm uninstall -g @dvcorg/cml');
-    } catch (err) {}
-  }
+    await exec('npm uninstall -g @dvcorg/cml');
+  } catch (err) {}
 
   try {
     core.info(`Intalling CML version ${version}`);
     await exec(
-      `sudo npm i -g @dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`
-    );
-  } catch (err) {
-    await exec(
       `npm i -g @dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`
     );
-  }
+  } catch (err) {}
 };
 
 (async () => {
