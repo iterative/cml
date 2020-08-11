fbe37ade5a8c019222edf4f0699db6a717548142
davidgortega <g.ortega.david@gmail.com>
2020-08-11 10:20:18 +0200

    no dvc

--- src/github-action.js
+++ src/github-action.js
@@ -3,28 +3,21 @@ const { exec } = require('./utils');
 
 const setup_cml = async () => {
   const version = core.getInput('version');
+
+  try {
+    core.info('Unintalling previous CML');
+    await exec('npm uninstall -g @dvcorg/cml');
+  } catch (err) {}
+
   core.info(`Intalling CML version ${version}`);
   await exec(
     `npm i -g @dvcorg/cml${version !== 'latest' ? `@${version}` : ''}`
   );
 };
 
-const setup_dvc = async () => {
-  const install = core.getInput('dvc_install');
-  const version = core.getInput('dvc_version');
-
-  if (install === 'true') {
-    core.info(`Intalling DVC version ${version}`);
-    await exec(
-      `pip install dvc[all]${version !== 'latest' ? `==${version}` : ''}`
-    );
-  }
-};
-
 (async () => {
   try {
     await setup_cml();
-    await setup_dvc();
   } catch (error) {
     core.setFailed(error.message);
   }
