1f05040293d36f4b26b9979c2f774a73af48b7e3
davidgortega <g.ortega.david@gmail.com>
2020-09-04 19:32:58 +0200

    always

--- bin/cml-cloud-runner-entrypoint.js
+++ bin/cml-cloud-runner-entrypoint.js
@@ -54,7 +54,7 @@ const shutdown_host = async () => {
   }
 };
 
-const shutdown = async error => {
+const shutdown = async (error) => {
   try {
     console.log('Unregistering runner');
 
@@ -137,7 +137,7 @@ const run = async () => {
 
   const proc = spawn(command, { shell: true });
 
-  proc.stderr.on('data', data => {
+  proc.stderr.on('data', (data) => {
     data && console.log(data.toString('utf8'));
 
     if (data && !IS_GITHUB) {
@@ -148,7 +148,7 @@ const run = async () => {
     }
   });
 
-  proc.stdout.on('data', async data => {
+  proc.stdout.on('data', async (data) => {
     data && console.log(data.toString('utf8'));
 
     if (data && IS_GITHUB && data.includes('Running job')) {
@@ -175,6 +175,6 @@ const run = async () => {
   }, 1000);
 };
 
-run().catch(err => {
+run().catch((err) => {
   shutdown(err);
 });
