aa76398cf60cd846a1949c9acb64722ecec5345b
Elle O'Brien <elleobrien11@gmail.com>
2020-10-07 23:51:14 +0000

    cml-send-comment.js edited online with Bitbucket

--- bin/cml-send-comment.js
+++ bin/cml-send-comment.js
@@ -7,13 +7,13 @@ const yargs = require('yargs');
 
 if (process.env.GITHUB_ACTIONS) {
   const { head_sha: HEAD_SHA, handle_error, comment } = require('../src/github');
-} else if (process.env.BITBUCKET_COMMIT) {
+} else if (process.env.CI) {
   const { head_sha: HEAD_SHA, handle_error, comment } = require('../src/bitbucket');
 } else {
   const { head_sha: HEAD_SHA, handle_error, comment } = require('../src/gitlab');
 }  
 
-console.log(process.env.BITBUCKET_COMMIT);
+console.log(process.env.CI);
 
 const run = async (opts) => {
   const { 'commit-sha': sha, 'head-sha': head_sha } = opts;
