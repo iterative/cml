5543229de3fda04a0f2086af02dd2d80cac6be02
elleobrien <elleobrien11@gmail.com>
2020-11-18 16:53:12 -0800

    multiple writes

--- src/drivers/bitbucket_cloud.js
+++ src/drivers/bitbucket_cloud.js
@@ -21,26 +21,31 @@ class BitBucketCloud {
     const { project_path } = this;
     const { commit_sha, report } = opts;
 
-    // Get the PR ID #
+    // Make a comment in the commit
+    const commit_endpoint = `/repositories/${project_path}/commit/${commit_sha}/comments/`;
+    const commit_body = JSON.stringify({ content: { raw: report } });
+    const commit_output = await this.request({
+      endpoint: commit_endpoint,
+      method: 'POST',
+      commit_body
+    });
+
+    // Check for a corresponding PR. If it exists, also put the comment there.
     const pr_endpt = `/repositories/${project_path}/commit/${commit_sha}/pullrequests`;
     const pr_out = await this.request({ endpoint: pr_endpt });
-    if (!pr_out)
-      throw new Error(
-        'CML was unable to find a Pull Request corresponding to your commit.'
-      );
-    const pr_id = pr_out.values[0].id;
+    if (pr_out) {
+      // Get PR ID
+      const pr_id = pr_out.values[0].id;
+      // Append a watermark to the report with a link to the commit
+      const commit_link = commit_sha.substr(0, 7);
+      const long_report = `${commit_link}\n  ${report}`;
+      const pr_body = JSON.stringify({ content: { raw: long_report } });
 
-    // Append a watermark to the report with a link to the commit
-    const commit_link = commit_sha.substr(0, 7);
-    const long_report = `${commit_link}\n  ${report}`;
-
-    // Write a comment on the PR
-    const endpoint = `/repositories/${project_path}/pullrequests/${pr_id}/comments`;
-    const body = JSON.stringify({ content: { raw: long_report } });
-
-    const output = await this.request({ endpoint, method: 'POST', body });
-
-    return output;
+      // Write a comment on the PR
+      const endpoint = `/repositories/${project_path}/pullrequests/${pr_id}/comments`;
+      await this.request({ endpoint, method: 'POST', body: pr_body });
+    }
+    return commit_output;
   }
 
   async check_create() {
