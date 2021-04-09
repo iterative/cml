6d4dd5c6322eab88b4a75524a288f436582bce48
davidgortega <g.ortega.david@gmail.com>
2021-04-09 12:53:14 +0200

    Boolean

--- src/drivers/gitlab.js
+++ src/drivers/gitlab.js
@@ -51,7 +51,7 @@ class Gitlab {
         })
     );
 
-    this._detected_base = possible_bases.find(String);
+    this._detected_base = possible_bases.find(Boolean);
     if (!this._detected_base) throw new Error('GitLab API not found');
 
     return this._detected_base;
