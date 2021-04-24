6dd94c42d34cc9c767b5df08143f0e86819f5c19
davidgortega <g.ortega.david@gmail.com>
2021-04-24 18:44:14 +0200

    branch

--- src/cml.js
+++ src/cml.js
@@ -280,6 +280,7 @@ class CML {
       }
 
       await exec(`git fetch --prune`);
+      console.log(await exec('git branch'));
       await exec(`git checkout -B ${source} ${sha}`);
       await exec(`git checkout -b ${target}`);
       await exec(`git add ${paths.join(' ')}`);
