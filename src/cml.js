cd2d32396c62c0acd2f858a1f79d057e86071658
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-05-27 22:55:30 +0200

    Rename error to err
    It was a lapsus

--- src/cml.js
+++ src/cml.js
@@ -222,10 +222,10 @@ class CML {
   async repo_token_check() {
     try {
       await this.runner_token();
-    } catch (error) {
-      if (error.message === 'Bad credentials')
-        error.message += ', REPO_TOKEN should be a personal access token';
-      throw error;
+    } catch (err) {
+      if (err.message === 'Bad credentials')
+        err.message += ', REPO_TOKEN should be a personal access token';
+      throw err;
     }
   }
 
