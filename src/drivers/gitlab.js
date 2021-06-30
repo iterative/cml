bda22ace9ff4392e688ccbb2f980b81a341672cd
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-06-30 14:32:18 +0200

    Add runner activity checks for GitLab

--- src/drivers/gitlab.js
+++ src/drivers/gitlab.js
@@ -185,14 +185,14 @@ class Gitlab {
       (runner) => runner.name === name || runner.description === name
     )[0];
 
-    if (runner) return { id: runner.id, name: runner.name };
+    if (runner) return { id: runner.id, name: runner.name, busy: runner.active, online: runner.status === 'online' };
   }
 
   async runnersByLabels(opts = {}) {
     const { labels } = opts;
     const endpoint = `/runners?per_page=100?tag_list=${labels}`;
     const runners = await this.request({ endpoint, method: 'GET' });
-    return runners.map((runner) => ({ id: runner.id, name: runner.name }));
+    return runners.map((runner) => ({ id: runner.id, name: runner.name, busy: runner.active, online: runner.status === 'online' }));
   }
 
   async prCreate(opts = {}) {
