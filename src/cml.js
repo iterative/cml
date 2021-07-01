2e5ffb108151b9bfb90c8153b57ac2d993d655f3
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-07-01 13:01:40 +0200

    Paremeterize remote on git fetch

--- src/cml.js
+++ src/cml.js
@@ -303,7 +303,7 @@ class CML {
         }
       }
 
-      await exec(`git fetch origin ${sha}`);
+      await exec(`git fetch ${remote} ${sha}`);
       await exec(`git checkout -B ${target} ${sha}`);
       await exec(`git checkout -b ${source}`);
       await exec(`git add ${paths.join(' ')}`);
