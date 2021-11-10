56d89c30053a946f54f5ca04c762b2e4c8c6c99b
DavidGOrtega <g.ortega.david@gmail.com>
2021-11-10 15:37:50 +0100

    log

--- src/drivers/bitbucket_cloud.js
+++ src/drivers/bitbucket_cloud.js
@@ -244,7 +244,7 @@ class BitbucketCloud {
   async request(opts = {}) {
     const { token, api } = this;
     const { url, endpoint, method = 'GET', body } = opts;
-    console.log(token);
+    console.log({ url, endpoint, method, body });
     if (!(url || endpoint))
       throw new Error('Bitbucket Cloud API endpoint not found');
     const headers = {
