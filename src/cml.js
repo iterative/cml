6bbccbb5dee069e47cfbef7aacf0706865bba35d
DavidGOrtega <g.ortega.david@gmail.com>
2022-05-30 12:44:58 +0200

    fix parselog

--- src/cml.js
+++ src/cml.js
@@ -261,73 +261,59 @@ class CML {
     let { data } = opts;
     if (!data) return;
 
+    data = data.toString('utf8');
+
     const date = new Date();
+    let log = {
+      level: 'info',
+      date: date.toISOString(),
+      repo: this.repo
+    };
 
-    try {
-      data = data.toString('utf8');
-
-      let log = {
-        level: 'info',
-        date: date.toISOString(),
-        repo: this.repo
-      };
-
-      if (this.driver === GITHUB) {
-        const id = 'gh';
-        if (data.includes('Running job')) {
-          log.job = id;
-          log.status = 'job_started';
-        } else if (data.includes('completed with result')) {
-          log.job = id;
-          log.status = 'job_ended';
-          log.success = data.includes('Succeeded');
-        } else if (data.includes('Listening for Jobs')) {
-          log.status = 'ready';
-        }
-
-        const [, message] = data.split(/[A-Z]:\s/);
-        return { ...log, message: (message || data).replace(/\n/g, '') };
+    if (this.driver === GITHUB) {
+      const id = 'gh';
+      if (data.includes('Running job')) {
+        log.job = id;
+        log.status = 'job_started';
+      } else if (data.includes('completed with result')) {
+        log.job = id;
+        log.status = 'job_ended';
+        log.success = data.includes('Succeeded');
+      } else if (data.includes('Listening for Jobs')) {
+        log.status = 'ready';
       }
-
-      if (this.driver === GITLAB) {
-        const { msg, job, duration_s: duration } = JSON.parse(data);
-        log = { ...log, job };
-
-        if (msg.endsWith('received')) {
-          log.status = 'job_started';
-        } else if (duration) {
-          log.status = 'job_ended';
-          log.success = msg.includes('Job succeeded');
-        } else if (msg.includes('Starting runner for')) {
-          log.status = 'ready';
-        }
-        return log;
-      }
-
-      if (this.driver === BB) {
-        const id = 'bb';
-        if (data.includes('Getting step StepId{accountUuid={')) {
-          log.job = id;
-          log.status = 'job_started';
-        } else if (
-          data.includes('Completing step with result Result{status=')
-        ) {
-          log.job = id;
-          log.status = 'job_ended';
-          log.success = data.includes('status=PASSED');
-        } else if (data.includes('Updating runner status to "ONLINE"')) {
-          log.status = 'ready';
-        }
-
-        log.level = log.success ? 'info' : 'error';
-        return log.status ? log : null;
-      }
-    } catch (err) {
-      winston.warn(`Failed parsing log: ${err.message}`);
-      winston.warn(
-        `Original log bytes, as Base64: ${Buffer.from(data).toString('base64')}`
-      );
     }
+
+    if (this.driver === GITLAB) {
+      const { msg, job, duration_s: duration } = JSON.parse(data);
+      log = { ...log, job };
+
+      if (msg.endsWith('received')) {
+        log.status = 'job_started';
+      } else if (duration) {
+        log.status = 'job_ended';
+        log.success = msg.includes('Job succeeded');
+      } else if (msg.includes('Starting runner for')) {
+        log.status = 'ready';
+      }
+    }
+
+    if (this.driver === BB) {
+      const id = 'bb';
+      if (data.includes('Getting step StepId{accountUuid={')) {
+        log.job = id;
+        log.status = 'job_started';
+      } else if (data.includes('Completing step with result Result{status=')) {
+        log.job = id;
+        log.status = 'job_ended';
+        log.success = data.includes('status=PASSED');
+      } else if (data.includes('Updating runner status to "ONLINE"')) {
+        log.status = 'ready';
+      }
+    }
+
+    log.level = log.success ? 'info' : 'error';
+    return log.status ? log : null;
   }
 
   async startRunner(opts = {}) {
