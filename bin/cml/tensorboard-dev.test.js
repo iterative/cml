08028f324f38a5f2f028804bb76a08abdc719ea3
Casper da Costa-Luis <casper.dcl@physics.org>
2022-06-14 14:12:34 +0100

    soft-deprecate --file (#1059)
    Co-authored-by: Helio Machado <0x2b3bfa0+git@googlemail.com>

--- bin/cml/tensorboard-dev.test.js
+++ bin/cml/tensorboard-dev.test.js
@@ -55,30 +55,28 @@ describe('CML e2e', () => {
     const output = await exec(`node ./bin/cml.js tensorboard-dev --help`);
 
     expect(output).toMatchInlineSnapshot(`
-"cml.js tensorboard-dev
+      "cml.js tensorboard-dev
 
-Get a tensorboard link
+      Get a tensorboard link
 
-Options:
-      --help          Show help                                        [boolean]
-      --version       Show version number                              [boolean]
-      --log           Maximum log level
-          [string] [choices: \\"error\\", \\"warn\\", \\"info\\", \\"debug\\"] [default: \\"info\\"]
-  -c, --credentials   TB credentials as json. Usually found at
-                      ~/.config/tensorboard/credentials/uploader-creds.json. If
-                      not specified will look for the json at the env variable
-                      TB_CREDENTIALS.                        [string] [required]
-      --logdir        Directory containing the logs to process.         [string]
-      --name          Tensorboard experiment title. Max 100 characters. [string]
-      --description   Tensorboard experiment description. Markdown format. Max
-                      600 characters.                                   [string]
-      --md            Output as markdown [title || name](url).         [boolean]
-  -t, --title         Markdown title, if not specified, param name will be used.
-                                                                        [string]
-  -f, --file          Append the output to the given file. Create it if does not
-                      exist.                                            [string]
-      --rm-watermark  Avoid CML watermark.                             [boolean]"
-`);
+      Options:
+            --help          Show help                                        [boolean]
+            --version       Show version number                              [boolean]
+            --log           Maximum log level
+                [string] [choices: \\"error\\", \\"warn\\", \\"info\\", \\"debug\\"] [default: \\"info\\"]
+        -c, --credentials   TB credentials as json. Usually found at
+                            ~/.config/tensorboard/credentials/uploader-creds.json. If
+                            not specified will look for the json at the env variable
+                            TB_CREDENTIALS.                        [string] [required]
+            --logdir        Directory containing the logs to process.         [string]
+            --name          Tensorboard experiment title. Max 100 characters. [string]
+            --description   Tensorboard experiment description. Markdown format. Max
+                            600 characters.                                   [string]
+            --md            Output as markdown [title || name](url).         [boolean]
+        -t, --title         Markdown title, if not specified, param name will be used.
+                                                                              [string]
+            --rm-watermark  Avoid CML watermark.                             [boolean]"
+    `);
   });
 
   test('cml tensorboard-dev --md returns md and after command TB is still up', async () => {
