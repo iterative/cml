cd5d7fe0990180b9f48dd2bc352db5bd1c0ee08c
Daniel Barnes <dabarnes2b@gmail.com>
2022-07-01 02:08:25 -0700

    review suggestions (#1083)
    * revert this
    
    * log warning verbage
    
    * revert this
    
    * move acpiSock to runLocal
    
    * connect info log
    
    * Revert "revert this"
    
    This reverts commit f4c0c0476b4570760bfa8f42b956cb137d8f7019.
    
    * Revert "revert this"
    
    This reverts commit 8a77fae6dbefb4058cda610806f4d10f0df336b8.
    
    * typo

--- bin/cml/runner.js
+++ bin/cml/runner.js
@@ -242,6 +242,24 @@ const runLocal = async (opts) => {
     await tf.saveTfState({ tfstate, path });
   }
 
+  if (process.platform === 'linux') {
+    const acpiSock = net.connect('/var/run/acpid.socket');
+    acpiSock.on('connect', () => {
+      winston.info('Connected to acpid service.');
+    });
+    acpiSock.on('error', (err) => {
+      winston.warn(
+        `Error connecting to ACPI socket: ${err.message}. The acpid.service helps with instance termination detection.`
+      );
+    });
+    acpiSock.on('data', (buf) => {
+      const data = buf.toString().toLowerCase();
+      if (data.includes('power') && data.includes('button')) {
+        shutdown({ ...opts, reason: 'ACPI shutdown' });
+      }
+    });
+  }
+
   const dataHandler = async (data) => {
     const logs = await cml.parseRunnerLog({ data, name });
     for (const log of logs) {
@@ -319,20 +337,6 @@ const run = async (opts) => {
   ['SIGTERM', 'SIGINT', 'SIGQUIT'].forEach((signal) => {
     process.on(signal, () => shutdown({ ...opts, reason: signal }));
   });
-
-  if (process.platform === 'linux') {
-    const acpiSock = net.connect('/var/run/acpid.socket');
-    acpiSock.on('error', (err) => {
-      winston.warn(
-        `Error setting ACPI socket: ${err.message}. Package acpi is a requirement.`
-      );
-    });
-    acpiSock.on('data', (buf) => {
-      const data = buf.toString().toLowerCase();
-      if (data.includes('power') && data.includes('button'))
-        shutdown({ ...opts, reason: 'ACPI shutdown' });
-    });
-  }
 
   opts.workdir = opts.workdir || `${homedir()}/.cml/${opts.name}`;
   const {
