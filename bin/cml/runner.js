c1d7cc4ba706cdedea502aa028e5da7f0192a448
Daniel Jimenez <devildan.knex@gmail.com>
2022-07-05 21:28:25 +1200

    chore: Fix linting rules

--- bin/cml/runner.js
+++ bin/cml/runner.js
@@ -402,7 +402,9 @@ const run = async (opts) => {
   }
 
   if (driver === 'github') {
-    winston.warn('Github Actions timeout has been updated from 72h to 35 days. Update your workflow accordingly to be able to restart it automatically.');
+    winston.warn(
+      'Github Actions timeout has been updated from 72h to 35 days. Update your workflow accordingly to be able to restart it automatically.'
+    );
   }
 
   winston.info(`Preparing workdir ${workdir}...`);
