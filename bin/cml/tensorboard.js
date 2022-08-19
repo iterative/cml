3c05aaa76ba053ebbafbd241e1b8c1b8e1ada3dc
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-08-19 12:24:25 +0000

    Hide driver/repo/token options in cml tensorboard

--- bin/cml/tensorboard.js
+++ bin/cml/tensorboard.js
@@ -2,6 +2,11 @@ exports.command = 'tensorboard';
 exports.description = 'Manage tensorboard.dev agents';
 exports.builder = (yargs) =>
   yargs
+    .options({
+      driver: { hidden: true },
+      repo: { hidden: true },
+      token: { hidden: true }
+    })
     .commandDir('./tensorboard', { exclude: /\.test\.js$/ })
     .recommendCommands()
     .demandCommand()
