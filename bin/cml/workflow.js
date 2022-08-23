5dd53fd30d6d6e441d68937fc8c8a1f02e74f4f9
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-08-23 15:25:49 +0200

    Apply suggestions from code review
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>

--- bin/cml/workflow.js
+++ bin/cml/workflow.js
@@ -1,5 +1,5 @@
 exports.command = 'workflow';
-exports.description = 'Manage continuous integration workflows';
+exports.description = 'Manage CI workflows';
 exports.builder = (yargs) =>
   yargs
     .commandDir('./workflow', { exclude: /\.test\.js$/ })
