4b67d61a566560bb01793f2dbe8418ba923ff997
Daniel Barnes <dabarnes2b@gmail.com>
2022-10-08 13:39:05 -0700

    bad ternary?

--- bin/cml/asset/publish.js
+++ bin/cml/asset/publish.js
@@ -15,8 +15,10 @@ exports.handler = async (opts) => {
     opts.native = true;
   }
 
-  const { file, repo, native, asset: path } = opts;
-  const cml = new CML({ ...opts, repo: native ? repo : 'cml' });
+  // const { file, repo, native, asset: path } = opts;
+  const { file, asset: path } = opts;
+  // const cml = new CML({ ...opts, repo: native ? repo : 'cml' });
+  const cml = new CML({ ...opts });
   const output = await cml.publish({ ...opts, path });
 
   if (!file) console.log(output);
