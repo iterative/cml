1d49da1e72ba5f7e8b5c7091a0d3183e81485800
Deepyaman Datta <deepyaman.datta@utexas.edu>
2022-10-24 20:06:17 -0400

    Update connect.test.js

--- bin/cml/tensorboard/connect.test.js
+++ bin/cml/tensorboard/connect.test.js
@@ -20,15 +20,15 @@ describe('CML e2e', () => {
             --help    Show help                                              [boolean]
 
       Options:
-        -c, --credentials   TensorBoard credentials as JSON, usually found at
-                            ~/.config/tensorboard/credentials/uploader-creds.json
+        -c, --credentials  TensorBoard credentials as JSON, usually found at
+                           ~/.config/tensorboard/credentials/uploader-creds.json
                                                                    [string] [required]
-            --logdir        Directory containing the logs to process          [string]
-            --name          Tensorboard experiment title; max 100 characters  [string]
-            --description   Tensorboard experiment description in Markdown format; max
-                            600 characters                                    [string]
-            --md            Output as markdown [title || name](url)          [boolean]
-        -t, --title         Markdown title, if not specified, param name will be used
+            --logdir       Directory containing the logs to process           [string]
+            --name         Tensorboard experiment title; max 100 characters   [string]
+            --description  Tensorboard experiment description in Markdown format; max
+                           600 characters                                     [string]
+            --md           Output as markdown [title || name](url)           [boolean]
+        -t, --title        Markdown title, if not specified, param name will be used
                                                                               [string]"
     `);
   });
