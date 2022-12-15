03e009fcca8e3a7de84ae588c42a46adaa5aa8af
Daniel Barnes <dabarnes2b@gmail.com>
2022-12-15 15:31:47 -0500

    `cml ci` fetch tags with depth/unshallow (#1269)
    * fetch tags when depth is 0/unshallow
    
    * Update src/cml.js
    
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>
    
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>

--- src/cml.js
+++ src/cml.js
@@ -517,10 +517,17 @@ class CML {
         if (
           (await exec('git', 'rev-parse', '--is-shallow-repository')) === 'true'
         ) {
-          return await exec('git', 'fetch', '--all', '--unshallow');
+          return await exec('git', 'fetch', '--all', '--tags', '--unshallow');
         }
       } else {
-        return await exec('git', 'fetch', '--all', '--depth', fetchDepth);
+        return await exec(
+          'git',
+          'fetch',
+          '--all',
+          '--tags',
+          '--depth',
+          fetchDepth
+        );
       }
     }
   }
