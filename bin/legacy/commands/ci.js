21a101d6bc6103025d2a3356dfe6be5a8f073da7
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-12-19 23:06:17 +0000

    Fix blunder introduced with #1288
    :old-man-yells-at-casper:

--- bin/legacy/commands/ci.js
+++ bin/legacy/commands/ci.js
@@ -1,5 +1,6 @@
-const { handler } = require('../../cml/repo/prepare');
+const { builder, handler } = require('../../cml/repo/prepare');
 
 exports.command = 'ci';
 exports.description = 'Prepare Git repository for CML operations';
 exports.handler = handler;
+exports.builder = builder;
