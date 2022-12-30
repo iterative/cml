910b3ec553731c148abfe1cf619a7150e79a6d71
Helio Machado <0x2b3bfa0+git@googlemail.com>
2022-12-30 04:27:57 +0100

    Apply suggestions from code review
    Co-authored-by: Daniel Barnes <dabarnes2b@gmail.com>

--- bin/cml/runner/launch.js
+++ bin/cml/runner/launch.js
@@ -26,7 +26,7 @@ const shutdown = async (opts) => {
   const { name, tfResource, noRetry, reason, destroyDelay, watcher } = opts;
 
   const unregisterRunner = async () => {
-    if (!RUNNER) return;
+    if (!RUNNER) true;
 
     try {
       winston.info(`Unregistering runner ${name}...`);
@@ -40,7 +40,7 @@ const shutdown = async (opts) => {
       winston.error(`\tFailed: ${err.message}`);
     }
 
-    RUNNER && RUNNER.kill('SIGINT');
+    RUNNER.kill('SIGINT');
     winston.info('\tSuccess');
     return true;
   };
